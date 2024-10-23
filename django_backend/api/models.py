c390dbd0510dae6a8cc792e6d795cae2c7e6370e
gentrum <carterdunaway@gmail.com>
2024-10-22 22:59:38 -0500

    Backend now correctly creates Tasks row and inserts into postgreSQL relation tasks.

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Task(models.Model):
+class Tasks(models.Model):
     name = models.CharField(max_length = 100)
-    description = models.TextField
+    description = models.TextField(default="")
     hours = models.IntegerField()
-    date = models.DateField()
+    duedate = models.DateField()
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'tasks'
 
     def __str__(self):
         return self.name
